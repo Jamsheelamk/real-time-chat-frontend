fd350101daead9f6900ab5029306a552b00f0381
Jamsheelamk <jamssheelamk@gmail.com>
2022-02-03 19:52:54 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AuthGuard } from './auth.guard';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatHomeComponent } from './components/chat/chat-home/chat-home.component';
+import { ChatRoomComponent } from './components/chat/chat-room/chat-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'userRegister',component:SignupComponent},
+  {path:'chat',component:ChatComponent,canActivate:[AuthGuard],
+   children: [{path:'',component:ChatHomeComponent},
+              {path:'room',component:ChatRoomComponent},
+  ]}
+                      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+
+import {HttpInterceptor, HTTP_INTERCEPTORS} from '@angular/common/http';
+
+
+// import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {SocketService} from '../app/services/socket.service';
+
+
+
+
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+import { AuthGuard } from './auth.guard';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatHomeComponent } from './components/chat/chat-home/chat-home.component';
+import { ChatRoomComponent } from './components/chat/chat-room/chat-room.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    ChatComponent,
+    ChatHomeComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    // SocketIoModule.forRoot()
+    
+  
+   
+    
+   
+    
+
   ],
-  providers: [],
+  providers: [AuthGuard,SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
